22adbe281b1857de8ee70820d0c1d10f51ee12cb
Jianhao54 <168104640+Jianhao54@users.noreply.github.com>
2024-05-24 23:48:09 +1000

    major project

--- sketch.js
+++ sketch.js
@@ -1,123 +1,60 @@
-//We need a variable to hold our image
-let img;
+var img;
+var WIDTH;
+var HEIGHT;
+var pixelSize = 10;  // 方块大小
+var totalPixels;
+var drawnPixels = 0;
+var pixelsToDraw;
 
-//We will divide the image into segments
-let numSegments = 50;
-
-//We will store the segments in an array
-let segments = [];
-
-//let's add a variable to switch between drawing the image and the segments
-let drawSegments = false;
-
-//Let's make an object to hold the draw properties of the image
-let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
-
-//And a variable for the canvas aspect ratio
-let canvasAspectRatio = 0;
-
-//let's load the image from disk
 function preload() {
-  img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
+  img = loadImage("assets/Claude_Monet,_Saint-Georges_majeur_au_crépuscule.jpg");
 }
 
 function setup() {
-  //We will make the canvas the same size as the image using its properties
-  createCanvas(windowWidth, windowHeight);
-  //let's calculate the aspect ratio of the image
-  imgDrwPrps.aspect = img.width / img.height;
-  //and the aspect ratio of the canvas
-  canvasAspectRatio = width / height;
-  calculateImageDrawProps();
-  //We can use the width and height of the image to calculate the size of each segment
-  let segmentWidth = img.width / numSegments;
-  let segmentHeight = img.height / numSegments;
-  /*
-  Divide the original image into segments, we are going to use nested loops
-  */
-
-  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
-    //this is looping over the height
-    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
-      //We will use the x and y position to get the colour of the pixel from the image
-      //let's take it from the centre of the segment
-      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
-       segments.push(segment);
+  WIDTH = img.width;
+  HEIGHT = img.height;
+  createCanvas(WIDTH, HEIGHT);
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  img.loadPixels();
+  
+  pixelsToDraw = new Array();
+  for (let y = 0; y < HEIGHT; y += pixelSize) {
+    for (let x = 0; x < WIDTH; x += pixelSize) {
+      pixelsToDraw.push({x: x, y: y});
     }
   }
+  shuffle(pixelsToDraw, true);
+  
+  totalPixels = pixelsToDraw.length;
+  frameRate(1000);
 }
 
 function draw() {
-  background(0);
-  if (drawSegments) {
-    //let's draw the segments to the canvas
-    for (const segment of segments) {
-      segment.draw();
+  for (var i = 0; i < 100; i++) {
+    if (drawnPixels >= totalPixels) {
+      noLoop();  // 停止绘制
+      break;
     }
-  } else {
-    //let's draw the image to the canvas
-    image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
-  }
-}
-function keyPressed() {
-  if (key == " ") {
-    //this is a neat trick to invert a boolean variable,
-    //it will always make it the opposite of what it was
-    drawSegments = !drawSegments;
+    var pixel = pixelsToDraw[drawnPixels];
+    var pix = img.get(pixel.x, pixel.y);
+    fill(pix);
+    rect(pixel.x, pixel.y, pixelSize, pixelSize);
+    drawnPixels++;
   }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-  calculateImageDrawProps();
+// Fisher-Yates Shuffle Algorithm
+function shuffle(array, shouldShuffle) {
+  if (!shouldShuffle) return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-function calculateImageDrawProps() {
 
-  canvasAspectRatio = width / height;
-  
-  //if the image is wider than the canvas
-  if (imgDrwPrps.aspect > canvasAspectRatio) {
-    //then we will draw the image to the width of the canvas
-    imgDrwPrps.width = width;
-    //and calculate the height based on the aspect ratio
-    imgDrwPrps.height = width / imgDrwPrps.aspect;
-    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
-    imgDrwPrps.xOffset = 0;
-  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
-    //otherwise we will draw the image to the height of the canvas
-    imgDrwPrps.height = height;
-    //and calculate the width based on the aspect ratio
-    imgDrwPrps.width = height * imgDrwPrps.aspect;
-    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
-    imgDrwPrps.yOffset = 0;
-  }
-  else if (imgDrwPrps.aspect == canvasAspectRatio) {
-    //if the aspect ratios are the same then we can draw the image to the canvas size
-    imgDrwPrps.width = width;
-    imgDrwPrps.height = height;
-    imgDrwPrps.xOffset = 0;
-    imgDrwPrps.yOffset = 0;
-  }
-}
-//Here is our class for the image segments, we start with the class keyword
-class ImageSegment {
 
-  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
-    this.srcImgSegXPos = srcImgSegXPosInPrm;
-    this.srcImgSegYPos = srcImgSegYPosInPrm;
-    this.srcImgSegWidth = srcImgSegWidthInPrm;
-    this.srcImgSegHeight = srcImgSegHeightInPrm;
-    this.srcImgSegColour = srcImgSegColourInPrm;
-  }
 
-  draw() {
-    //let's draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
-    stroke(0);
-    fill(this.srcImgSegColour);
-    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
-  }
-}
